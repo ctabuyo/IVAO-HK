29a8192b34bca6e7e964933f0ee64813bdd17aa4
Cristian Tabuyo <cristian@tabuyo.es>
2017-02-01 19:50:19 +0100

    Starting to work...

---
+++ Pods/Target Support Files/Pods-IVAO-HK/Pods-IVAO-HK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IVAO_HK : NSObject
+@end
+@implementation PodsDummy_Pods_IVAO_HK
+@end
---
+++ Pods/Target Support Files/Pods-IVAO-HKTests/Pods-IVAO-HKTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IVAO_HKTests : NSObject
+@end
+@implementation PodsDummy_Pods_IVAO_HKTests
+@end
